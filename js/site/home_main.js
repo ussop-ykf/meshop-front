c1cdf7d0abbab9db08e872ba123510296426b7bf
ussop <2754216911@qq.com>
2024-01-05 22:21:27 +0800

    修改合并bug

--- js/site/home_main.js
+++ js/site/home_main.js
@@ -17,11 +17,11 @@ require(['jquery','jquery_SuperSlide','handlebar','jquery_url','./common','home'
 		//加载登录用户信息
 		common.getUserInfo();
 		//用户登出
-		// common.loginOut();
+		common.loginOut();
 		//1.加载产品分类
 		// home.getParam();
 		//2.加载热销商品
-		// home.getHotProduct();
+		home.getHotProduct();
 		//3.加载楼层信息
 		// home.getFloors();
 	});	
