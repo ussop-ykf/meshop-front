9dc71d7f0a8129e967d8be9fda64d4d465328662
MrXuL2233 <3263971860@qq.com>
2024-01-05 20:40:34 +0800

    qq

---
+++ js/site/detail_main.js
@@ -0,0 +1,20 @@
+require.config({
+    paths: {
+            "jquery": "../jquery-1.6.2.min",
+            "jquery_SuperSlide": "../jquery.SuperSlide.2.1.1",
+            "handlebar": "../handlebars-v4.0.11",
+            "jquery_url": "../jquery.url",
+            },
+    shim:  {
+            'jquery_SuperSlide':['jquery'],
+            'jquery_url':['jquery'],
+            }
+});
+
+require(['jquery','jquery_SuperSlide','handlebar','jquery_url','./common','detail'],
+       function (jquery,jquery_SuperSlide,handlebar,jquery_url,common,detail){
+   $(function(){
+    alert("aa");
+       detail.getGoodDetail();
+   });	
+});
--- js/site/home_main.js
+++ js/site/home_main.js
@@ -15,13 +15,13 @@ require(['jquery','jquery_SuperSlide','handlebar','jquery_url','./common','home'
 		function (jquery,jquery_SuperSlide,handlebar,jquery_url,common, home){
 	$(function(){
 		//加载登录用户信息
-		common.getUserInfo();
+		// common.getUserInfo();
 		//用户登出
 		// common.loginOut();
 		//1.加载产品分类
 		// home.getParam();
 		//2.加载热销商品
-		// home.getHotProduct();
+		home.getHotProduct();
 		//3.加载楼层信息
 		// home.getFloors();
 	});	
