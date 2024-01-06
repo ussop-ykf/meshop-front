d397d8c9656565324341e5e33263746fc037f669
MrXuL2233 <3263971860@qq.com>
2024-01-06 11:14:13 +0800

    商品详情页以及index商品列表bug修改以及购物车的编写

--- js/site/product_detail_main.js
+++ js/site/product_detail_main.js
@@ -1,17 +1,23 @@
 require.config({
 	paths:{
-		"jquery":"/mall/dy2/js/jquery-1.6.2.min",
-		"handlebar": "/mall/dy2/js/handlebars-v4.0.11",
+		"jquery": "../jquery-1.6.2.min",
+            "jquery_SuperSlide": "../jquery.SuperSlide.2.1.1",
+            "handlebar": "../handlebars-v4.0.11",
+            "jquery_url": "../jquery.url",
 	},
+	shim:  {
+		'jquery_SuperSlide':['jquery'],
+		'jquery_url':['jquery'],
+		}
 });
 
 
 require(['jquery','./common','handlebar','product_detail'],function(jquery, common, handlebar, product_detail){
 	$(function(){
-		//加载登录用户信息
+		// 加载登录用户信息
 		common.getUserInfo();
-		//用户登出
-		common.loginOut();
+		// 用户登出
+		// common.loginOut();
 		product_detail.ready();
 	});
 });
